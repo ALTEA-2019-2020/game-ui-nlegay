c776261adf72ec970fdba4d252ece609eaf1b021
Nicolas Legay <nicolas.legay.etu@univ-lille.fr>
2020-02-03 09:40:59 +0100

    first part

---
+++ src/main/java/com/miage/altea/game_ui/controller/IndexController.java
@@ -0,0 +1,26 @@
+package com.miage.altea.game_ui.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @PostMapping("/registerTrainer")
+    ModelAndView registerNewTrainer(@PathVariable String trainerName){
+        var mav = new ModelAndView();
+
+        mav.addObject("name", trainerName);
+        mav.setViewName("register");
+
+        return mav;
+    }
+}

8b4c3025d30cc950dcb0d542132c9f11c413c526
Nicolas Legay <nicolas.legay.etu@univ-lille.fr>
2020-03-02 08:25:40 +0100

    gestion du Locale du navigateur

--- src/main/java/com/miage/altea/game_ui/pokemonTypes/service/PokemonTypeServiceImpl.java
+++ src/main/java/com/miage/altea/game_ui/pokemonTypes/service/PokemonTypeServiceImpl.java
@@ -3,6 +3,11 @@ package com.miage.altea.game_ui.pokemonTypes.service;
 import com.miage.altea.game_ui.pokemonTypes.bo.PokemonType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,13 +20,12 @@ public class PokemonTypeServiceImpl implements PokemonTypeService {
     private String pokemonServiceUrl;
 
     public List<PokemonType> listPokemonsTypes() {
-        var result = restTemplate.getForObject(pokemonServiceUrl+"pokemon-types/", PokemonType[].class);
-        return List.of(result);
-    }
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(HttpHeaders.ACCEPT_LANGUAGE, String.valueOf(LocaleContextHolder.getLocale()));
+        HttpEntity entity = new HttpEntity(headers);
 
-    public List<PokemonType> listPokemonsTypesB() {
-        var result = restTemplate.getForObject(pokemonServiceUrl+"pokemon-types/", PokemonType[].class);
-        return List.of(result);
+        ResponseEntity<PokemonType[]> response = restTemplate.exchange(pokemonServiceUrl+"pokemon-types/", HttpMethod.GET, entity, PokemonType[].class);
+        return List.of(response.getBody());
     }
 
     @Autowired
--- src/test/java/com/miage/altea/game_ui/pokemonTypes/service/PokemonTypeServiceImplTest.java
+++ src/test/java/com/miage/altea/game_ui/pokemonTypes/service/PokemonTypeServiceImplTest.java
@@ -2,8 +2,11 @@ package com.miage.altea.game_ui.pokemonTypes.service;
 
 import com.miage.altea.game_ui.pokemonTypes.bo.PokemonType;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Matchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -25,8 +28,19 @@ class PokemonTypeServiceImplTest {
         pikachu.setName("pikachu");
         pikachu.setId(25);
 
-        var expectedUrl = "http://localhost:8080/pokemon-types";
-        when(restTemplate.getForObject(expectedUrl, PokemonType[].class)).thenReturn(new PokemonType[]{pikachu});
+        var expectedUrl = "http://localhost:8080/pokemon-types/";
+
+        //when(restTemplate.exchange(expectedUrl, HttpMethod.GET, new HttpEntity(new HttpHeaders()), PokemonType[].class).getBody()).thenReturn(new PokemonType[]{pikachu});
+
+        ResponseEntity<PokemonType[]> entity = new ResponseEntity<>(new PokemonType[]{pikachu}, HttpStatus.ACCEPTED);
+        when(restTemplate.exchange(
+                ArgumentMatchers.eq(expectedUrl),
+                ArgumentMatchers.eq(HttpMethod.GET),
+                ArgumentMatchers.any(),
+                ArgumentMatchers.eq(PokemonType[].class)
+        )).thenReturn(entity);
+
+        //when(restTemplate.getForObject(expectedUrl, PokemonType[].class)).thenReturn(new PokemonType[]{pikachu});
 
         var pokemons = pokemonServiceImpl.listPokemonsTypes();
 
